a3f2bc7b1a717968afe32d32ad1f591ca84c2d9c
Juan Cruz Viotti <juanchiviotti@gmail.com>
2015-09-30 11:31:01 -0400

    Implement --watch option
    When this boolean option is passed, the directory is watched for
    changes.

--- build/sync.js
+++ build/sync.js
@@ -1,4 +1,6 @@
-var chalk, resin, rsync, shell, _;
+var Promise, chalk, resin, rsync, shell, tree, _;
+
+Promise = require('bluebird');
 
 _ = require('lodash');
 
@@ -9,6 +11,8 @@ resin = require('resin-sdk');
 rsync = require('./rsync');
 
 shell = require('./shell');
+
+tree = require('./tree');
 
 module.exports = {
   signature: 'sync <uuid> [source]',
@@ -30,9 +34,15 @@ module.exports = {
       boolean: true,
       description: 'show progress',
       alias: 'p'
+    }, {
+      signature: 'watch',
+      boolean: true,
+      description: 'watch files',
+      alias: 'w'
     }
   ],
   action: function(params, options, done) {
+    var performSync;
     if (options.ignore != null) {
       options.ignore = _.words(options.ignore);
     }
@@ -41,23 +51,40 @@ module.exports = {
     });
     process.chdir(params.source);
     console.info("Connecting with: " + params.uuid);
+    performSync = function() {
+      return Promise["try"](function() {
+        if (options.before != null) {
+          return shell.runCommand(options.before);
+        }
+      }).then(function() {
+        var command;
+        command = rsync.getCommand(_.merge(params, options));
+        console.info('Running command...');
+        console.log(chalk.cyan(command));
+        return shell.runCommand(command);
+      }).tap(function() {
+        return console.info('Synced, restarting device');
+      }).then(function() {
+        return resin.models.device.restart(params.uuid);
+      });
+    };
     return resin.models.device.isOnline(params.uuid).tap(function(isOnline) {
       if (!isOnline) {
         throw new Error('Device is not online');
       }
-    }).then(function() {
-      if (options.before != null) {
-        return shell.runCommand(options.before, params.source);
+    }).then(performSync).then(function() {
+      var watch;
+      if (options.watch) {
+        watch = tree.watch(params.source);
+        watch.on('watching', function(watcher) {
+          return console.info("Watching path: " + watcher.path);
+        });
+        watch.on('change', function(type, filePath) {
+          console.info("- " + (type.toUpperCase()) + ": " + filePath);
+          return performSync()["catch"](done);
+        });
+        return watch.on('error', done);
       }
-    }).then(function() {
-      var command;
-      command = rsync.getCommand(_.merge(params, options));
-      console.log(chalk.cyan(command));
-      return shell.runCommand(command);
-    }).tap(function() {
-      return console.info('Synced, restarting device');
-    }).then(function() {
-      return resin.models.device.restart(params.uuid);
     }).nodeify(done);
   }
 };
